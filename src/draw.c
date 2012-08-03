4122c01ef2e332834cf2c4b66591521b39ac3b5a
Josiah Worcester <josiahw@gmail.com>
2012-08-03 14:52:07 -0600

    Some more optimization of screen drawing.

--- src/draw.c
+++ src/draw.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <string.h>
 
 #include "constants.h"
 #include "draw.h"
@@ -7,7 +8,6 @@ uint32_t framebuf[240][320];
 
 static void unsafe_draw_pixel(uint32_t x, uint32_t y, uint32_t col)
 {
-
 	if((col & 0xFF000000) ^ 0xFF000000) return;
 	framebuf[y][x] = col;
 }
@@ -29,17 +29,17 @@ static void draw_tile(int32_t *m, int32_t tile, int32_t px, int32_t py)
 	int x_pre = 0;
 	int x_post = 0;
 	int y_pre = 0;
-	int x_lim = 8;
-	int y_lim = 8;
+	int x_lim = px+8;
+	int y_lim = py+8;
 	int x_str = px;
 	int y_str = py;
 
-	if(8 + px > 320) {
-		x_lim = 320 - px;
-		x_post = 8 - x_lim;
+	if(x_lim > 320) {
+		x_lim = 320;
+		x_post = 8 + px - 320;
 	}
 	if(8 + py > 240) {
-		y_lim = 240 - py;
+		y_lim = 240;
 	}
 	if(px < 0) {
 		x_str = 0;
@@ -51,9 +51,9 @@ static void draw_tile(int32_t *m, int32_t tile, int32_t px, int32_t py)
 	}
 
 	i += y_pre;
-	for(int y = y_str; y < y_lim + py; y++) {
+	for(int y = y_str; y < y_lim; y++) {
 		i += x_pre;
-		for(int x = x_str; x < x_lim + px; x++)
+		for(int x = x_str; x < x_lim; x++)
 			unsafe_draw_pixel(x, y, m[i++]);
 		i += x_post;
 	}
@@ -110,9 +110,11 @@ static void draw_grid(int32_t *m, int zbit)
 
 void draw(int32_t *m)
 {
-	for(int y = 0; y < 240; y++)
-		for(int x = 0; x < 320; x++)
-			framebuf[y][x] = m[CL];
+	for(int x = 0; x < 320; x++)
+		framebuf[0][x] = m[CL];
+
+	for(int y = 1; y < 240; y++)
+		memcpy(framebuf[y], framebuf[0], sizeof(framebuf[0]));
 
 	draw_grid(m, 0);
 
